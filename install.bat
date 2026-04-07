09efa56eea9b19a86bb93b7d27a36f85f53993e4
Brett Phinney <brettphinney@CBS-GC1414-MINI.local>
2026-04-07 12:02:33 -0700

    fix: bat always deletes old ps1 before re-downloading fresh copy

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Downloading STAN installer...
+del "%~dp0install_stan.ps1" >nul 2>&1
 powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/bsphinney/stan/main/install_stan.ps1' -OutFile '%~dp0install_stan.ps1' -UseBasicParsing; & '%~dp0install_stan.ps1'"
 pause
